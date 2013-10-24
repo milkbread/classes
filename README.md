403fc896e2cdb8bb8f449506e7fa36f59612a00d
milkbread <klammer@klammer-ubuntu.(none)>
2013-10-24 15:43:14 +0200

    initial upload

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 classes
 =======
 
-Python-classes that help to evaluate the input from command line
+Python-functions that help to evaluate the input from command line
+
+* **getIOFiles**
+
+	- *Parameters:* -i <inputfile> -o <outputfile>'
+	- *Usage:* not used so far (used as dummy-function)
+
+* **getIOFilesObject**
+
+	- *Parameters:* -i <inputfile> -o <outputfile> -f <feature>'
+	- *Usage:* Build GeoJSON-Features from TopoJSON-Features
+	- *Examples:* https://github.com/milkbread/TopoJSON-Arcs/blob/master/makeGeoJSON.py
+
+* **getIOFilesRoute**
+
+	- *Parameters:* -o <outputfile> -s <start> -e <end>'
+	- *Usage:* Convert Json-Route-Features provided by [Cloudmade routing service](http://cloudmade.com/documentation/routing)
+	- *Examples:* https://github.com/milkbread/SnakesSmoothing/blob/master/routeToJson.py
+
+
+* **getIOFilesThreshold**
+
+	- *Parameters:* -i <inputfile> -o <outputfile> -t <threshold>'
+	- *Usage:* Simplify some geometries, needs a threshold (or tolerance), so you have to request that too
+	- *Examples:* 
+		* https://github.com/milkbread/SnakesSmoothing/blob/master/smoothToFeatClass.py
+		* https://github.com/milkbread/Visvalingam-Wyatt/blob/master/simplify.py
+
+*Current version: 0.2*
---
+++ checkArguments.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+#Python-functions that help to evaluate the input from command line
+#written by Ralf Klammer, 24.10.2013
+
+import getopt
+import sys
+
+def version():
+	return 0.2
+
+def getIOFiles(): 
+	inputfile = ''
+	outputfile = ''
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],"hi:o:",["ifile=","ofile="])
+	except getopt.GetoptError:
+		print 'test.py -i <inputfile> -o <outputfile>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'test.py -i <inputfile> -o <outputfile'
+			sys.exit()
+		elif opt in ("-i", "--ifile"):
+			inputfile = arg
+		elif opt in ("-o", "--ofile"):
+			outputfile = arg
+	
+	#print 'Input file is ', inputfile, len(inputfile)
+	#print 'Output file is ', outputfile, len(outputfile)
+
+	if(len(inputfile)==0 or len(outputfile)==0):
+		print 'You have to define in- and outputfile!!!'
+		sys.exit()
+	else:
+		return inputfile, outputfile;	
+
+def getIOFilesObject(): 
+	inputfile = ''
+	outputfile = ''
+	feature = ''
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],"hi:o:f:",["ifile=","ofile=", "feature="])
+	except getopt.GetoptError:
+		print 'test.py -i <inputfile> -o <outputfile> -f <feature>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'test.py -i <inputfile> -o <outputfile -f <feature>'
+			sys.exit()
+		elif opt in ("-i", "--ifile"):
+			inputfile = arg
+		elif opt in ("-o", "--ofile"):
+			outputfile = arg
+		elif opt in ("-f", "--feature"):
+			feature = arg
+	
+	#print 'Input file is ', inputfile, len(inputfile)
+	#print 'Output file is ', outputfile, len(outputfile)
+
+	if(len(inputfile)==0 or len(outputfile)==0):
+		print 'You have to define in- and outputfile and feature!!!'
+		sys.exit()
+	else:
+		return inputfile, outputfile, feature;	
+
+def getIOFilesRoute(): 
+	outputfile = ''
+	startcoord = ''
+	endcoord = ''
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],"ho:s:e:",["ofile=","start=","end="])
+	except getopt.GetoptError:
+		print 'test.py -o <outputfile> -s <start> -e <end>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'test.py -o <outputfile> -s <start> -e <end>'
+			print 'Offer coordinates in that form: <latitude, longitude>'
+			sys.exit()
+		elif opt in ("-o", "--ofile"):
+			outputfile = arg
+		elif opt in ("-s", "--start"):
+			startcoord = arg
+		elif opt in ("-e", "--end"):
+			endcoord = arg
+	
+	#print 'Input file is ', inputfile, len(inputfile)
+	#print 'Output file is ', outputfile, len(outputfile)
+
+	if(len(outputfile)==0 or len(startcoord)==0 or len(endcoord)==0):
+		print 'You have to define outputfile and the coordinates of start- & endpoints <latitude, longitude>!!!'
+		sys.exit()
+	else:
+		return outputfile, startcoord, endcoord;	
+
+def getIOFilesThreshold(): 
+	inputfile = ''
+	outputfile = ''
+	threshold = '0.1';
+	try:
+		opts, args = getopt.getopt(sys.argv[1:],"hi:o:t:",["ifile=","ofile=", "threshold="])
+	except getopt.GetoptError:
+		print 'test.py -i <inputfile> -o <outputfile> -t <threshold>'
+		sys.exit(2)
+	for opt, arg in opts:
+		if opt == '-h':
+			print 'test.py -i <inputfile> -o <outputfile -t <threshold>'
+			sys.exit()
+		elif opt in ("-i", "--ifile"):
+			inputfile = arg
+		elif opt in ("-o", "--ofile"):
+			outputfile = arg
+		elif opt in ("-t", "--threshold"):
+			threshold = arg
+
+	#print 'Input file is ', inputfile, len(inputfile)
+	#print 'Output file is ', outputfile, len(outputfile)
+
+	if(len(inputfile)==0 or len(outputfile)==0):
+		print 'You have to define in- and outputfile!!!'
+		sys.exit()
+	else:
+		print 'Threshold = ', threshold
+		return inputfile, outputfile, threshold;	
